d78b1f337247476919f46a03b0a7cea343a727f9
IntegralX1 <49178751+IntegralX1@users.noreply.github.com>
2019-09-09 20:49:18 -0600

    Review of CPSC1517

---
+++ ChinookSolution/ChinookSystem.Data/Entities/Album.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookSystem.Data.Entities
+{
+    public class Album
+    {
+        [Key]
+        public int AlbumId { get; set; }
+        public string Title { get; set; }
+        public int ArtistId { get; set; }
+        public int ReleaseYear { get; set; }
+
+        private string _ReleaseLable;
+
+        public string ReleaseLable
+        {
+            get
+            {
+                return _ReleaseLable;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    _ReleaseLable = null;
+                }
+                else
+                {
+                    _ReleaseLable = value;
+                }
+            }
+        }
+
+        public virtual Artist Artist { get; set; }
+
+    }
+}
---
+++ ChinookSolution/ChinookSystem.Data/Entities/Artist.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region additional namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace ChinookSystem.Data.Entities
+{
+    [Table("Artists")]
+    public class Artist
+    {
+        //[Key, Column(Order = n)] //n indicates the order of the fields on the DB table.
+        //[Key, DatabaseGenerated(DatabaseGeneratedOption.Computed - identity - none)] ---> If you have a character string then select NONE. 
+        //This option can be used on a field that is not a primary key. Computer so the framework does not expect that there will be data supplied.
+        [Key]
+        public int ArtistId { get; set; }
+
+        //Fully implemented property for a nullable field.
+        private string _Name; 
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    _Name = null;
+                }
+                else
+                {
+                    _Name = value;
+                }
+            }
+        }
+
+        public virtual ICollection<Album> Albums { get; set; }
+
+    }//eoc
+}//eon
